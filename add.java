196dc580645a89ff5c69eb50216d5bac1d1d5747
Devopsnhc09 <devopsnhc09@gmail.com>
2023-02-28 13:56:33 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+class add
+{
+	public static void main(String []args)
+	{ 
+		int num1, num2, sum;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First Number:");
+		num1 = Integer.parseInt(args[0]);
+		System.out.println("Enter Second Number:");
+		num2 = Integer.parseInt(args[1]);
+		
+		sum = num1 + num2;
+		System.out.println("sum of these numbers: "+sum);
+	}
+}
